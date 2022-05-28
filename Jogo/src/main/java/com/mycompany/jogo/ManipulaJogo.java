a037f25b17a5525eab26f2e4c44a84a062db519d
Viasdeski <vitor.viasdeski@hotmail.com>
2022-05-28 16:23:07 -0300

    Jogo verdadeiro ou Falos com Radio Button em Jframe

---
+++ Jogo/src/main/java/com/mycompany/jogo/ManipulaJogo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.mycompany.jogo;
+
+/**
+ *
+ * @author Vitor
+ */
+public interface ManipulaJogo {
+    
+    public abstract void enabledBotao( boolean sim_nao);
+    public void verificaSelecao();
+          
+}
